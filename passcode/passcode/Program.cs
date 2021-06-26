fc30a03d94891258eb878a866f5605a4c5b162f7
sumit kumar <sumitsliet1830070@gmail.com>
2021-06-26 22:24:33 +0530

    added passcode

---
+++ passcode/passcode/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace passcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the passcode?");
+            var code = Console.ReadLine();
+
+            if (code == "secret")
+            {
+                Console.WriteLine("Authenticted");
+
+            }
+            else if (code != "secret")
+            {
+                Console.WriteLine("Not Authenticted");
+            }
+               
+            
+        }
+    }
+}
