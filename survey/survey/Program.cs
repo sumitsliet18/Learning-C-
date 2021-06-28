39a8300eff92471f5d3d6510089be8df4d54f95d
sumit kumar <sumitsliet1830070@gmail.com>
2021-06-28 21:59:15 +0530

    Updated survey

--- survey/survey/Program.cs
+++ survey/survey/Program.cs
@@ -9,6 +9,12 @@ namespace survey
             Console.WriteLine("What is your name?");
             var name = Console.ReadLine();
 
+            if (name == "")
+            {
+                Console.WriteLine("You did not write anything, please try again");
+                name = Console.ReadLine();
+            }
+
             Console.WriteLine("What is your age?");
             var age = Console.ReadLine();
 
