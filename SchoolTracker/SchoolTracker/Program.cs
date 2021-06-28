3c648865a75f985b1df32203d2cf8981ade659a9
sumit kumar <sumitsliet1830070@gmail.com>
2021-06-28 19:51:57 +0530

    Added SchoolTracker

---
+++ SchoolTracker/SchoolTracker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SchoolTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
