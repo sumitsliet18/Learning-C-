c32ffbb5b7b83a13e80b23cd447ae42811c4eb1c
sumit kumar <sumitsliet1830070@gmail.com>
2021-06-28 20:09:45 +0530

    Updated SchoolTracker

--- SchoolTracker/SchoolTracker/Program.cs
+++ SchoolTracker/SchoolTracker/Program.cs
@@ -6,7 +6,9 @@ namespace SchoolTracker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var studentGrades = new int[10] {80, 77, 54, 85, 45, 65, 48, 78, 48, 89};
+            
+            Console.WriteLine(studentGrades[4]);
         }
     }
 }
